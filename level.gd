f64eb5a7d4cee49c3b2e95227c63cc04ccdab0aa
Peter W Jones <pwj2012@gmail.com>
2025-03-15 18:25:51 -0700

    stuff

--- level.gd
+++ level.gd
@@ -2,6 +2,13 @@ extends Node2D
 
 const ACORN_SCENE = preload("res://acorn.tscn")
 
+@export var number_of_acorns := 0
+@export var number_of_water := 0
+
+func _ready() -> void:
+	Globals.acorns = number_of_acorns
+	Globals.water = number_of_water
+
 func _input(event: InputEvent) -> void:
 	if (event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_RIGHT
 		and event.pressed == true and Globals.acorns > 0):
