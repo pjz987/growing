0ec17935195f639493d45bee40c2f30dc4e162e4
Al Burns <al.burns.email@gmail.com>
2025-03-15 21:54:50 -0700

    tree pattern example
    here's an example of making tree shapes auto-growing with each acorn drop

--- tree_base.gd
+++ tree_base.gd
@@ -1,20 +1,33 @@
 class_name TreeBase
 extends AbstractTree
 
+static var tree_shape: Array[Vector2] = [
+	Vector2(0, -1),
+	Vector2(0, -1),
+	Vector2(0, -1),
+	Vector2(1, -1),
+	Vector2(0, 1),
+	Vector2(1, -1),
+]
+var tree_index := 0
 var tree_trunks: Array[StaticBody2D] = []
 var my_trunk_maker = TrunkMaker.new()
 
+func get_grow_vector():
+	if tree_index + 2 > tree_shape.size():
+		return Vector2(0, 0)
+	else:
+		tree_index += 1
+		return tree_shape[tree_index]
+
+func _ready():
+	_create_trunk()
+
 func _create_trunk():
 	var new_trunk = my_trunk_maker.make_trunk(position, self)
+	new_trunk.grow()
 	tree_trunks.append(new_trunk)
 	input_pickable = false
-
-func silly() -> void:
-	position.x += 3
-	rotate(-.0625)
-	for one_static_body in tree_trunks:
-		one_static_body.position.x += 3
-		one_static_body.rotate(-.0625)
 		
 func _input(event: InputEvent) -> void:
 	super._input(event)
--- tree_trunk.gd
+++ tree_trunk.gd
@@ -3,24 +3,36 @@ extends AbstractTree
 
 var tree_base: StaticBody2D
 var my_trunk_maker = TrunkMaker.new()
+var finished_growing := false
 
-func _ready() -> void:
-	grow()
-
+func grow():
+	var next_pos: Vector2 = tree_base.get_grow_vector()
+	print(next_pos)
+	if next_pos != Vector2(0, 0):
+		is_growing = true
+		var tween = get_tree().create_tween()
+		tween.connect("finished", _grow_finished)
+		tween.tween_property(self, "position", next_pos * 12 + position, 1)
+	else:
+		finished_growing = true
+	print("finished")
+	
 func _create_trunk():
 	var new_trunk = my_trunk_maker.make_trunk(position, tree_base)
 	input_pickable = false
-	tree_base.silly()
 	tree_base.tree_trunks.append(new_trunk)
+	new_trunk.grow()
 
 func _input(event: InputEvent) -> void:
-	super._input(event)
+	pass
 
 func _on_mouse_entered() -> void:
 	super._on_mouse_entered()
 
 func _grow_finished():
 	super._grow_finished()
+	if not finished_growing:
+		_create_trunk()
 
 func _on_mouse_exited() -> void:
 	super._on_mouse_exited()
