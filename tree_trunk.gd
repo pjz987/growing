f654b11208a10012e4a3a37a28fc5a4de0c4825f
Al Burns <al.burns.email@gmail.com>
2025-03-15 18:33:24 -0700

    broke up trees into classes
    the trees now consist of the abstract tree class (not implemented), the tree base class which has an array of trunks, and the tree trunk class which is instantiated with each growth and tracks the base class. each tree instance tracks when it is growing and clicks don't cause growth. the tree base as a temporary 'silly' function that causes the tree to shift around

--- tree_trunk.gd
+++ tree_trunk.gd
@@ -1,25 +1,22 @@
-class_name TreeBase
+class_name TreeTrunk
 extends AbstractTree
 
-var tree_trunks: Array[StaticBody2D] = []
+var tree_base: StaticBody2D
 const TREE_TRUNK_SCENE = preload("res://tree_trunk.tscn")
+
+func _ready() -> void:
+	grow()
 
 func _create_trunk():
 	var new_trunk := Utils.instantiate_scene_on_level(TREE_TRUNK_SCENE, global_position)
-	new_trunk.tree_base = self
-	tree_trunks.append(new_trunk)
+	new_trunk.tree_base = tree_base
 	input_pickable = false
-	
-func silly() -> void:
-	position.x += 3
-	rotate(-.0625)
-	for one_static_body in tree_trunks:
-		one_static_body.position.x += 3
-		one_static_body.rotate(-.0625)
-		
+	tree_base.silly()
+	tree_base.tree_trunks.append(new_trunk)
+
 func _input(event: InputEvent) -> void:
 	super._input(event)
-	
+
 func _on_mouse_entered() -> void:
 	super._on_mouse_entered()
 
