3d1ed02411021490d1be345b7738b2f4ef84dc1b
Peter W Jones <pwj2012@gmail.com>
2025-03-20 14:42:56 -0700

    :goal:

--- level.gd
+++ level.gd
@@ -5,6 +5,8 @@ const ACORN_SCENE = preload("res://acorn.tscn")
 @export var number_of_acorns := 0
 @export var number_of_water := 0
 
+@onready var goal_label: Label = $Goal/GoalLabel
+
 func _ready() -> void:
 	Globals.acorns = number_of_acorns
 	Globals.water = number_of_water
@@ -16,3 +18,15 @@ func _input(event: InputEvent) -> void:
 		print(event)
 		Globals.acorns -= 1
 		Utils.instantiate_scene_on_level(ACORN_SCENE, event.position)
+
+
+#func _on_goal_area_entered(area: Area2D) -> void:
+	#goal_label.visible = true
+#
+#
+func _on_goal_body_entered(body: Node2D) -> void:
+	goal_label.visible = true
+
+
+func _on_goal_area_shape_entered(area_rid: RID, area: Area2D, area_shape_index: int, local_shape_index: int) -> void:
+	goal_label.visible = true
