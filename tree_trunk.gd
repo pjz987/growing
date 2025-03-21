a10579ad3b1f584b463dd2a24265e3253dcb5393
Al Burns <al.burns.email@gmail.com>
2025-03-21 10:12:16 -0700

    got tree sprout/trunk/top working
    in this version there is a bug where you can see the tree trunk behind the tree top right when it starts growing. also, the sprout was turning into a trunk and growing too quickly, so I threw in some awkward code into the TreeBase._input function that sets a .2 second timeout

--- tree_trunk.gd
+++ tree_trunk.gd
@@ -3,11 +3,14 @@ extends AbstractTree
 
 var tree_base: StaticBody2D
 var my_trunk_maker = TrunkMaker.new()
-
+const TREE_TRUNK = preload("res://assets/tree-trunk.png")
+const TREE_TOP = preload("res://assets/tree-top.png")
 func _ready() -> void:
+	$Sprite2D.texture = TREE_TOP
 	grow()
 
 func _create_trunk():
+	$Sprite2D.texture = TREE_TRUNK
 	var new_trunk = my_trunk_maker.make_trunk(position, tree_base)
 	input_pickable = false
 	tree_base.tree_trunks.append(new_trunk)
