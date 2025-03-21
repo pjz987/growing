1968cc7220b591f4f0ebe9de2b1f488811e8e587
Peter W Jones <pwj2012@gmail.com>
2025-03-21 16:13:32 -0700

    art stuff

--- level.gd
+++ level.gd
@@ -9,6 +9,7 @@ const ACORN_SCENE = preload("res://acorn.tscn")
 
 @onready var goal_label: Label = $Goal/GoalLabel
 @onready var blackout_layer: ColorRect = $BlackoutLayer
+@onready var blackout_layer_2: ColorRect = $BlackoutLayer2
 
 var next_level_scene: PackedScene
 
@@ -30,5 +31,9 @@ func _input(event: InputEvent) -> void:
 func _on_goal_body_entered(body: Node2D) -> void:
 	goal_label.visible = true
 	blackout_layer.visible = true
+	blackout_layer_2.visible = true
 	await get_tree().create_tween().tween_property(blackout_layer, "color:a", 1.0, fade_in_out_time).finished
+	await get_tree().create_timer(1.0).timeout
+	await get_tree().create_tween().tween_property(blackout_layer_2, "color:a", 1.0, fade_in_out_time).finished
+	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_packed(next_level_scene)
