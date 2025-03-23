f190af57a53694c274f5f609e156d5382f95f677
Al Burns <al.burns.email@gmail.com>
2025-03-23 05:16:34 -0700

    added controls overlay

--- ui.gd
+++ ui.gd
@@ -2,7 +2,9 @@ extends Label
 
 func _ready() -> void:
 	text = "Acorns: " + str(Globals.acorns) + "  Water: " + str(Globals.water)
+	text += "\nF1 for Controls"
 	Globals.refresh_text.connect(refresh_text)
 
 func refresh_text() -> void:
 	text = "Acorns: " + str(Globals.acorns) + "  Water: " + str(Globals.water)
+	text += "\nF1 for Controls"
